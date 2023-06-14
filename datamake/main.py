96f7d4973f2fcf72a0541542717e89ba27089b3b
wanbiguizhao <shiweishiweihubugui@live.cn>
2023-06-14 15:09:54 +0800

    adjust function name

--- datamake/main.py
+++ datamake/main.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 from paddleocr import PaddleOCR
 
 from sklearn.model_selection import train_test_split
-
-
+from image_tools import drawImage
+import shutil
 import re
 def parser_core_data(ocr_data):
     ocr_data=ocr_data[0]
@@ -29,6 +29,7 @@ def parser_core_data(ocr_data):
             code_name=match.groupdict()["code_name"]
             for cn in re.split("、| ",code_name):
                 info_dict["code_name"].add(cn)
+    
     return info_dict
     
     
@@ -125,5 +126,91 @@ def batch_parser_ocr_data():
     print(c,len(glob(f"{basic_image_dir}/*.json")))
     #parser_core_data(ocrdata)
     #print(ocrdata)
-batch_infer_images()
-batch_parser_ocr_data()
+
+
+def batch_parser_ocr_data_fake_data():
+    #批量解析ocr数据,并且根据数据生成假数据图片
+    def save_data(code_name_pairs,short_name_pairs,save_path):
+        for index in tqdm(range(len(code_name_pairs))):
+            docunt_data=defaultdict(list)
+            for code in code_name_pairs[index]:
+                docunt_data["code_name"].append({f"cn":code})
+            for short in short_name_pairs[index]:
+                docunt_data["short_name"].append({f"sn":short})
+            image_path=f"{'_'.join(code_name_pairs[index])}.png"
+            split_char=" "
+            prob=random.random()
+            if prob<0.3:
+                split_char=" "
+            elif prob<0.7:
+                split_char="、"
+            else:
+                split_char="  "
+            drawImage(split_char.join(code_name_pairs[index]),split_char.join(short_name_pairs[index]),f"{save_path}/{'_'.join(code_name_pairs[index])}.png")
+            all_gt_docunt_data.append(
+                {
+                    "ground_truth":json.dumps({"gt_parse":docunt_data},ensure_ascii=False),
+                    "file_name":image_path
+                }
+            )
+        dump_jsonl(all_gt_docunt_data,f"{save_path}/metadata.jsonl")
+    def get_fake_data(k_num=20):
+        short_name_list=random.choices(gt_docunt_data["short_name"],k=k_num)
+        code_name_list=random.choices(gt_docunt_data["code_name"],k=k_num)
+        code_name_pairs=list(permutations(code_name_list,2))
+        
+        short_name_pairs=list(permutations(short_name_list,2))
+        #random.shuffle(short_name_pairs) 
+        return code_name_pairs,short_name_pairs
+    def smart_makedirs(dir_path):
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+        os.makedirs(dir_path)
+    c=0
+    all_gt_docunt_data=[]
+    gt_docunt_data=defaultdict(list)
+    for json_path in tqdm(glob(f"{basic_image_dir}/*.json")):
+        file_name=json_path.split("/")[-1]
+        prefix_file_name,ext=os.path.splitext(file_name)
+        with open(json_path,"r") as jsonfile:
+            ocr_data=json.load(jsonfile)
+            info_dict=parser_core_data(ocr_data)
+
+            #info_dict=defaultdict()
+            if len(info_dict["full_name"])!=1:
+                c=c+1
+                continue
+            if len(info_dict["short_name"])==0 or len(info_dict["code_name"])==0:
+                # print(ocr_data)
+                # print(info_dict)
+                c=c+1
+                continue
+            info_dict["full_name"]=[]
+            
+            for key_name,values in info_dict.items():
+                for v in values:
+                    if len(v)>6 or len(v)<3:
+                        continue
+                    gt_docunt_data[key_name].append(v)
+    from itertools import permutations
+    import random 
+    # 生成train的数据
+    
+
+
+    train_data_path=f"{basic_image_dir}/fake/train/"
+    val_data_path=f"{basic_image_dir}/fake/val/"
+    smart_makedirs(train_data_path)
+    smart_makedirs(val_data_path)
+    code_name_pairs,short_name_pairs=get_fake_data(k_num=30)
+    save_data(code_name_pairs,short_name_pairs,train_data_path)
+    code_name_pairs,short_name_pairs=get_fake_data(k_num=10)
+    save_data(code_name_pairs,short_name_pairs,val_data_path)
+    # 生成val的数据
+
+if __name__=="__main__":
+    #batch_infer_images()
+    #
+    batch_parser_ocr_data()
+    #batch_parser_ocr_data_fake_data()
+    pass
